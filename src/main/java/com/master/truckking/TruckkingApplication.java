66bd59b5f175bb9645d56b1b1d9d507c1608f52d
karuppusamy <nmkkannan@gmail.com>
2018-05-16 18:48:17 +0530

    Initial commit

---
+++ src/main/java/com/master/truckking/TruckkingApplication.java
@@ -0,0 +1,12 @@
+package com.master.truckking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckkingApplication.class, args);
+	}
+}
